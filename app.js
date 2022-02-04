eda1d86790a1360c078b1a58c0526580ede41f9b
CristianKB89 <cristianiksl@gmail.com>
2022-02-04 02:09:10 -0500

    Agregandole estilos

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3030;
+const path = require('path');
+
+/* app.use(express.static('public')); */
+
+app.use('/static',express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.listen(port,() => {
+    console.log('Servidor corriendo en el puerto '+ port);
+});
